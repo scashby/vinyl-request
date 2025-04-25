93f42ad8c76f364345de0c65678ef72855012479
Stephen Ashby <stephenashby@macmini.lan>
2025-04-25 13:54:27 -0400

    fix browsealbum154

--- src/components/BrowseCollectionPage.js
+++ src/components/BrowseCollectionPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import BrowseAlbums from './BrowseAlbums';
+import { supabase } from '../supabaseClient';
 
 const BrowseCollectionPage = () => {
   const { eventId } = useParams();
