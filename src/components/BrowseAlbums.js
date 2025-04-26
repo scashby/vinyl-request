e69c38535a8a1400471dcfe063a45cd5285f84c6
Stephen Ashby <stephenashby@macmini.lan>
2025-04-26 12:19:01 -0400

    proxy fetch 1218

--- src/components/BrowseAlbums.js
+++ src/components/BrowseAlbums.js
@@ -43,7 +43,7 @@ const BrowseAlbums = ({
           let updatedAlbum = { ...album };
           console.log('Processing album:', album.artist, album.title, 'Image URL:', album.image_url);
 
-          if (album.image_url === 'no') {
+          if (!album.image_url) {
             console.log('Fetching cover for:', album.artist, album.title);
             const fetchedImageUrl = await fetchAlbumCoverWithFallbacks(album.artist, album.title);
             const imageStatus = fetchedImageUrl ? 'yes' : 'no';
